a18ae05c1069508a3e23ec997e38f2ff99abb39b
Ted Naleid <contact@naleid.com>
2023-01-07 22:36:47 -0600

    validate commands

--- src/esquepkg/commands.nim
+++ src/esquepkg/commands.nim
@@ -77,9 +77,9 @@ iterator topicIterator(kcatOutput: string, broker: string): Topic =
 
 proc getBrokerTopics(
     self: ShellContext, broker: string, topicFilter: string): seq[Topic] =
-  let kcatCommand: string = fmt"kcat -L -b {broker}"
-  # todo, extract this out and make it a little higher level
-  let (output, exitCode) = self.exec(ShellCommand(command: kcatCommand))
+
+  let kcatCommand = self.kcat & @["-L", "-b", broker]
+  let (output, exitCode) = self.exec(kcatCommand)
 
   if exitCode != 0:
     # TODO throw an exception instead?
@@ -91,23 +91,34 @@ proc getBrokerTopics(
 
   result = toSeq(topicIterator(output, broker))
 
-# proc findSingleTopic(
-#     self: ShellContext, broker: string, topicFilter: string): TopicQueryResult =
-#   let topics = getBrokerTopics(self, broker, topicFilter)
-#   result = case topics.len:
-#     of 0: TopicQueryResult(kind: None)
-#     of 1: TopicQueryResult(kind: Single, topic: topics[0])
-#     else: TopicQueryResult(kind: Multi, topics: topics)
+proc findSingleTopic(
+    self: ShellContext, broker: string, topicFilter: string): MatchResult =
+  let topics = getBrokerTopics(self, broker, topicFilter)
+  result = case topics.len:
+    of 0: MatchResult(kind: None)
+    of 1: MatchResult(kind: Single, topic: topics[0])
+    else: MatchResult(kind: Multi, topics: topics)
 
-# proc catTopic(self: ShellContext, broker: string, topicFilter: string) =
-#   # we'll want to do something other than just output here...
-#   echo "cat topic"
+proc catTopic(self: ShellContext, command: EsqueCommand) =
+  # we'll want to do something other than just output here...
+  echo "cat topic"
+  let cmd = self.kcat & @["-C", "-e", "-q", "-b", command.env, "-t", command.topic] & command.remainingArgs
+  echo $cmd
+  echo self.exec(cmd).output
 
+proc firstTopic(self: ShellContext, command: EsqueCommand) =
+  let cmd = self.kcat & @["-C", "-e", "-q", "-b", command.env, "-t", command.topic, "-c", "1"] & command.remainingArgs
+  echo $cmd
+  echo self.exec(cmd).output
 
 proc runCommand*(self: ShellContext, command: EsqueCommand) =
   # for commands that want a specific topic, we could resolve that first
   # or peel off the commands that don't want a specific topic (env, list, help, partition, version)
   case command.kind:
+    of Cat:
+      self.catTopic(command)
+    of First:
+      self.firstTopic(command)
     of Env:
       echo "Running Env " & $command
     of List:
@@ -119,12 +130,15 @@ proc runCommand*(self: ShellContext, command: EsqueCommand) =
     of Version:
       echo "Running Version" & $command
     else:
-      # let topicQueryResult = findSingleTopic(self, command.env, command.topic)
+      echo fmt"TODO {command}"
 
-      let kcatCommand: string = fmt"kcat -C -b {command.env}"
-      echo fmt"kcatCommand {kcatCommand}"
 
 when isMainModule:
   let shellContext = buildShellContext()
   shellContext.runCommand(EsqueCommand(kind: List, env: "esque-kafka:9092"))
-  shellContext.runCommand(EsqueCommand(kind: Cat, env: "esque-kafka:9092"))
+  shellContext.runCommand(EsqueCommand(kind: Cat, env: "esque-kafka:9092", 
+    topic: "ten-partitions-lz4",
+    remainingArgs: @["-c", "1", "-p", "0"]))
+  shellContext.runCommand(EsqueCommand(kind: First, env: "esque-kafka:9092", 
+    topic: "ten-partitions-lz4",
+    remainingArgs: @["-p", "0", "-f", "'%k %p %o\n'"]))
--- src/esquepkg/shell.nim
+++ src/esquepkg/shell.nim
@@ -1,35 +1,75 @@
-import osproc, utils
+import osproc, utils, strformat, strutils
 
 type
   ShellContext* = ref object
     verbose*: bool
-    execCommand*: proc(self: ShellContext,
-      shellCommand: ShellCommand): tuple[output: string, exitCode: int]
+    execCommand*: proc(self: ShellContext, shellCommand: ShellCommand): 
+      tuple[output: string, exitCode: int]
+    kcat*: ShellCommand
+    kafkaConsumerGroups*: ShellCommand
+    kafkaTopics*: ShellCommand
+    kafkaAcls*: ShellCommand
   ShellCommand* = ref object
-    command*: string
+    command*: seq[string]
+
+proc `$`*(self: ShellCommand): string = $self.command
+
+proc `&`*(originalCommand: ShellCommand, moreArgs: seq[string]): ShellCommand =
+  ShellCommand(command: originalCommand.command & moreArgs)
+
+proc verifyExists(exePath: string): bool = 
+  execCmd(fmt"""command -v "{exePath}" 2>&1""") == 0
 
 proc execCommand(self: ShellContext, shellCommand: ShellCommand):
     tuple[output: string, exitCode: int] =
-  if self.verbose: log "+ " & shellCommand.command
-  result = execCmdEx(shellCommand.command)
+  # TODO switch this to the version that takes a seq
+  if self.verbose: log "+ " & shellCommand.command.join(" ")
+  result = execCmdEx(shellCommand.command.join(" "))
 
 template exec*(self: ShellContext, shellCommand: ShellCommand):
     tuple[output: string, exitCode: int] =
   self.execCommand(self, shellCommand)
 
-proc buildShellContext*(verbose: bool = false): ShellContext =
-  result = ShellContext(execCommand: execCommand, verbose: verbose)
+proc kcatCommand(verifyExists: proc(exePath: string): bool): ShellCommand = 
+  result = if verifyExists("kcat"):
+    ShellCommand(command: @["kcat"])
+  elif verifyExists("docker"):
+    ShellCommand(command: @["docker", "run", "edenhill/kcat:1.7.1", "kcat"])
+  else: 
+    when defined(macosx):
+      log "'kcat' is required. e.g. brew install kcat"
+    else: # assuming linux for now
+      log "install kcat or docker"
+    raiseAssert "Unable to find 'kcat' executable"
+
+
+proc buildShellContext*(
+  verbose: bool = false,
+  verifyExists: proc(exePath: string): bool = verifyExists,
+  execCommand = execCommand): ShellContext =
+
+  # todo verify that we've got the right executables here and stick them in the context
+  # later we can potentially build things up from config
+
+
+  # TODO start here, see if we get kcat like we want and that we can then call it
+
+  result = ShellContext( 
+    verbose: verbose, 
+    execCommand: execCommand,
+    kcat: verifyExists.kcatCommand)
 
 when isMainModule:
-  let shellCommand = ShellCommand(command: "ls -la")
+  let shellCommand = ShellCommand(command: @["ls", "-la"])
   let realShellContext = ShellContext(execCommand: execCommand, verbose: true)
   let realResult = realShellContext.exec(shellCommand)
   echo "exitCode: " & $realResult.exitCode
   echo realResult.output
 
-  proc execStub(shellContext: ShellContext, shellCommand: ShellCommand): tuple[
-      output: string, exitCode: int] =
-    result = (output: "fake output for " & shellCommand.command, exitCode: -1)
+  proc execStub(shellContext: ShellContext, shellCommand: ShellCommand): 
+      tuple[output: string, exitCode: int] =
+    result = (output: "fake output for " & shellCommand.command.join(" "), 
+      exitCode: -1)
 
   let stubbedShellContext = ShellContext(execCommand: execStub)
 
